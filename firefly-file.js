52268fd8ddcca245be431d905b4cdd6c2849199c
ishansubedi <ishan@appmozart.com>
2020-08-10 12:15:09 -0700

    added file upload from via browse flow

--- firefly-file.js
+++ firefly-file.js
@@ -1,17 +1,11 @@
-import { PolymerElement,html } from '@polymer/polymer/polymer-element.js';
-import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
-
-import '@polymer/paper-input/paper-input.js';
-import '@polymer/paper-fab/paper-fab.js';
-
-import '@firefly-elements/polymerfire/firebase-auth';
-import '@firefly-elements/polymerfire/firebase-storage-multiupload';
-import '@firefly-elements/polymerfire/firebase-storage-upload-task';
-
-import './firefly-icons/firefly-icons.js'
-
-
-
+import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
+import { afterNextRender } from "@polymer/polymer/lib/utils/render-status.js";
+import "@polymer/paper-input/paper-input.js";
+import "@polymer/paper-fab/paper-fab.js";
+import "@firefly-elements/polymerfire/firebase-auth";
+import "@firefly-elements/polymerfire/firebase-storage-multiupload";
+import "@firefly-elements/polymerfire/firebase-storage-upload-task";
+import "./firefly-icons/firefly-icons.js";
 /**
  * `firefly-file` This component is responsible for loading a single file into the
  * specified file storage location. To add it to your page, set the 'app-name' and
@@ -29,9 +23,8 @@ class FireflyFile extends PolymerElement {
    * String providing the tag name to register the element under.
    */
   static get is() {
-    return 'firefly-file';
+    return "firefly-file";
   }
-
   static get template() {
     return html`
       <style>
@@ -41,24 +34,22 @@ class FireflyFile extends PolymerElement {
           --icon-color: #2f466f;
           --icon-background: transparent;
         }
-
         paper-fab {
           --iron-icon-width: var(--icon-size);
           --iron-icon-height: var(--icon-size);
-          margin-right: 10px;
-          margin-top: 10px;
-          padding: 5px;
+          margin: 5px auto;
         }
-
         label {
           color: #909090;
         }
+        .file-upload-input {
+          display: none;
+        }
       </style>
-
-      <paper-fab icon="aspen:upload"></paper-fab><label>[[label]]</label>
-
+      <paper-fab icon="aspen:upload" on-click="__onFileIconClick"></paper-fab
+      ><label>[[label]]</label>
       <firebase-auth app-name="[[appName]]"></firebase-auth>
-      
+      <input class="file-upload-input" type="file" on-change="__fileSelected" />
       <firebase-storage-multiupload
         app-name="[[appName]]"
         path="[[path]]"
@@ -68,7 +59,6 @@ class FireflyFile extends PolymerElement {
         log=""
       >
       </firebase-storage-multiupload>
-
       <template is="dom-repeat" items="[[uploadTasks]]">
         <firebase-storage-upload-task
           app-name="[[appName]]"
@@ -84,7 +74,6 @@ class FireflyFile extends PolymerElement {
       </template>
     `;
   }
-
   /**
    * Object describing property-related metadata used by Polymer features
    */
@@ -93,50 +82,42 @@ class FireflyFile extends PolymerElement {
       /** The name of the application. */
       appName: {
         type: String,
-        value: ''
+        value: ""
       },
-
       /** The path to the storage location. i.e. '/path/to/directory' used to upload the file to a specific directory*/
       path: {
         type: String,
-        value: ''
+        value: ""
       },
-
       files: {
         type: Array,
         value: [],
         notify: true
       },
-
       uploadTasks: {
         type: Array,
         value: [],
         notify: true
       },
-
       /** The completely qualified download URL used to retrieve the file. */
       downloadUrl: {
         type: String,
-        value: ''
+        value: ""
       },
-
       label: {
         type: String,
-        value: 'Drop file on icon to upload'
+        value: "Drop file on icon to upload"
       },
-
       state: {
         type: String,
-        value: ''
+        value: ""
       },
-
       tempDownload: {
         type: String,
-        value: ''
+        value: ""
       }
     };
   }
-
   /**
    * Instance of the element is created/upgraded. Use: initializing state,
    * set up event listeners, create shadow dom.
@@ -145,16 +126,13 @@ class FireflyFile extends PolymerElement {
   constructor() {
     super();
   }
-
   /**
    * Use for one-time configuration of your component after local DOM is initialized.
    */
   ready() {
     super.ready();
-
     afterNextRender(this, function() {});
   }
-
   /**
    * Called every time the element is inserted into the DOM. Useful for
    * running setup code, such as fetching resources or rendering.
@@ -162,72 +140,74 @@ class FireflyFile extends PolymerElement {
    */
   connectedCallback() {
     super.connectedCallback();
-    let icon = this.shadowRoot.querySelector('paper-fab');
-    icon.addEventListener('drop', e => this.__fileSelected(e));
-    icon.addEventListener('dragenter', e => this.__cancelHandler(e));
-    icon.addEventListener('dragover', e => this.__cancelHandler(e));
+    let icon = this.shadowRoot.querySelector("paper-fab");
+    icon.addEventListener("drop", e => this.__fileSelected(e));
+    icon.addEventListener("dragenter", e => this.__cancelHandler(e));
+    icon.addEventListener("dragover", e => this.__cancelHandler(e));
   }
-
   /**
    * Called every time the element is removed from the DOM. Useful for
    * running clean up code (removing event listeners, etc.).
    */
   disconnectedCallback() {
     super.disconnectedCallback();
-    let icon = this.shadowRoot.querySelector('paper-fab');
-    icon.removeEventListener('drop', e => this.__fileSelected(e));
-    icon.removeEventListener('dragenter', e => this.__cancelHandler(e));
-    icon.removeEventListener('dragover', e => this.__cancelHandler(e));
+    let icon = this.shadowRoot.querySelector("paper-fab");
+    icon.removeEventListener("drop", e => this.__fileSelected(e));
+    icon.removeEventListener("dragenter", e => this.__cancelHandler(e));
+    icon.removeEventListener("dragover", e => this.__cancelHandler(e));
   }
-
   __cancelHandler(e) {
     e.preventDefault();
   }
-
   /**
    *
    */
   __stateChanged(state) {
-    console.log(state);
-    if (state == 'success') {
+    if (state == "success") {
       //this.set('downloadUrl', tempDownload);
       this.dispatchEvent(
-        new CustomEvent('show-msg', {
+        new CustomEvent("show-msg", {
           bubbles: true,
           composed: true,
           detail: {
-            msg: 'Upload complete'
+            msg: "Upload complete"
           }
         })
       );
     }
   }
-
+  /**
+   * handles the click event on upload icon
+   */
+  __onFileIconClick() {
+    this.shadowRoot.querySelector(".file-upload-input").click();
+  }
+  /**
+   * handles the file select event
+   * @param {Object} e the file select event
+   */
   __fileSelected(e) {
     e.preventDefault();
     e.stopPropagation();
-    console.log(e);
-
-    let file = e.dataTransfer.files[0];
-
+    let file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
     // upload the file
     const storage = this.shadowRoot.querySelector(
-      'firebase-storage-multiupload'
+      "firebase-storage-multiupload"
     );
     storage.ref
       .child(file.name)
       .put(file)
       .then(snapshot => {
-        if (snapshot.state == 'success') {
+        if (snapshot.state == "success") {
           return snapshot.ref.getDownloadURL(); // Will return a promise with the download link
         }
       })
       .then(downloadURL => {
-        let msg = '';
+        let msg = "";
         try {
-          this.set('downloadUrl', downloadURL);
+          this.set("downloadUrl", downloadURL);
           this.dispatchEvent(
-            new CustomEvent('file-upload-complete', {
+            new CustomEvent("file-upload-complete", {
               bubbles: true,
               composed: true,
               detail: {
@@ -235,14 +215,13 @@ class FireflyFile extends PolymerElement {
               }
             })
           );
-          msg = 'Icon updated';
+          msg = "Icon updated";
         } catch (error) {
           console.error(error);
-          msg = 'An error occurred while updating the icon.';
+          msg = "An error occurred while updating the icon.";
         }
-
         this.dispatchEvent(
-          new CustomEvent('show-msg', {
+          new CustomEvent("show-msg", {
             bubbles: true,
             composed: true,
             detail: {
@@ -253,5 +232,4 @@ class FireflyFile extends PolymerElement {
       });
   }
 }
-
 window.customElements.define(FireflyFile.is, FireflyFile);
